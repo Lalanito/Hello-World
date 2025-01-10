6a5857eae83567a06310dccbb10dd3b8dd01635b
Lalanito <lalannebustillo@gmail.com>
2025-01-10 17:28:40 +0100

    Version 0.1
    hoy he quedado con mi friend

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {/*
+
+        hola, q tal t va la vida?
+        muy bien y tu?
+        super bien y q vas a hacer esta tarde xq podemos quedar
+        ps perfecto
+        
+    */
+
+print("Hello, World");
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
